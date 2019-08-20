27934ea743572b5eaae53bd52075c1993a709f48
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-08-20 12:40:34 -0500

    reorder projects

--- convert-to-pdf.sh
+++ convert-to-pdf.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-cat projects/*.md > portfolio.md
+touch portfolio.md
+
+cat projects/chess-pipeline.md >> portfolio.md
+cat projects/idac-predictor.md >> portfolio.md
+cat projects/spotify-genre-playlists.md >> portfolio.md
+cat projects/typing-rpg.md >> portfolio.md
+cat projects/genetic-hello-world.md >> portfolio.md
+cat projects/markov-dracula.md >> portfolio.md
 
 pandoc portfolio.md \
     -o portfolio.pdf \
