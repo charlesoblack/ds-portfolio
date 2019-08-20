2676a4a305ef9eed2c74ea7a69fe0dcee0f567c0
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-08-20 09:58:03 -0500

    bash script for converting to pdf with pandoc

---
+++ convert-to-pdf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in ./projects/*.md
+do
+    pandoc $i \
+        -o ${i%.md}.pdf \
+        -fmarkdown-implicit_figures \
+        -V geometry:margin=2cm \
+        -f gfm
+done
