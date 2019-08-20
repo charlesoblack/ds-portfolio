94894703230109b3d9d55cf2f386b3db40c76da3
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-08-20 10:40:29 -0500

    combine all .md files before piping to pandoc

--- convert-to-pdf.sh
+++ convert-to-pdf.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-for i in ./projects/*.md
-do
-    pandoc $i \
-        -o ${i%.md}.pdf \
-        -fmarkdown-implicit_figures \
-        -V geometry:margin=2cm \
-        --css pandoc.css
-done
+portfolio=$( cat *.md )
+
+pandoc $portfolio \
+    -o portfolio.pdf \
+    -fmarkdown-implicit_figures \
+    -V geometry:margin=2cm \
+    --css pandoc.css
